eb34542dd65b90b481f42ef21eaf82f865768769
Xingjun Ma <danxjma@gmail.com>
2022-03-23 15:36:12 +0800

    Create other_rankings.sql

---
+++ influence ranking/influence ranking/other_rankings.sql
@@ -0,0 +1,75 @@
+ResponseRank:
++-----------+-------+-----------------+
+| friend_id | count | screen_name     |
++-----------+-------+-----------------+
+| 21129105  |   476 | bomani_jones    |
+| 14173315  |   375 | NBCNews         |
+| 28785486  |   354 | ABC             |
+| 18823758  |   297 | BlkSportsOnline |
+| 5392522   |   228 | nprnews         |
+| 16664681  |   175 | latimes         |
+| 2735591   |   167 | FastCompany     |
+| 26403610  |   164 | InTheBleachers  |
+| 15838676  |   164 | Lana            |
+| 22215485  |   155 | wxbrad          |
+| 24191521  |   153 | joeovies        |
+| 16028034  |   145 | rodimusprime    |
+| 2883841   |   137 | eonline         |
+| 15382667  |   126 | DianneG         |
+| 28918980  |   124 | greensboro_nc   |
+| 16018479  |   121 | ShannonGrissom  |
+| 11616202  |   120 | JayCaruso       |
+| 69147333  |   120 | KegsnEggs       |
+| 3497941   |   119 | owillis         |
+| 14777850  |   116 | JohnLusher      |
++-----------+-------+-----------------+
+FollowerRank:
++-----------+-------+----------------+
+| friend_id | count | screen_name    |
++-----------+-------+----------------+
+| 65589378  |  1380 | MySOdotCom     |
+| 15081182  |  1348 | WarrenWhitlock |
+| 5392522   |  1251 | nprnews        |
+| 30924802  |  1122 | JeffreyPJacobs |
+| 10202     |  1120 | chrisbrogan    |
+| 15131310  |  1110 | WholeFoods     |
+| 37877645  |  1077 | awakeningaimee |
+| 44171091  |  1045 | tedcoine       |
+| 15014405  |  1032 | PleasureEllis  |
+| 65577676  |  1031 | FixedOpsGenius |
+| 31891798  |  1023 | FredCuellar    |
+| 2735591   |   995 | FastCompany    |
+| 6123762   |   979 | DanSchawbel    |
+| 11294522  |   972 | JeanetteJoy    |
+| 20192882  |   961 | mikewhitmore   |
+| 24733117  |   938 | jilevin        |
+| 15582157  |   933 | JohnAguiar     |
+| 16201819  |   919 | AMAnet         |
+| 5683952   |   915 | tommytrc       |
+| 31471175  |   907 | vitalyvt       |
++-----------+-------+----------------+
++-----------+-----------------+-----------------+
+| id        | followers_count | screen_name     |
++-----------+-----------------+-----------------+
+| 2883841   |         6058788 | eonline         |
+| 16827333  |         4499479 | souljaboy       |
+| 15131310  |         3613273 | WholeFoods      |
+| 28785486  |         2938746 | ABC             |
+| 144380789 |         2601560 | greysonchance   |
+| 46919513  |         2591942 | williecolon     |
+| 26787673  |         2264464 | CocaCola        |
+| 7861312   |         2204427 | feliciaday      |
+| 5392522   |         2192819 | nprnews         |
+| 2884771   |         2131889 | Newsweek        |
+| 12133382  |         1838216 | PBS             |
+| 15290441  |         1781516 | WWEUniverse     |
+| 6446742   |         1685391 | ufc             |
+| 6173842   |         1660548 | Cheezburger     |
+| 22679419  |         1609400 | ESQTV           |
+| 15808765  |         1448638 | CoryBooker      |
+| 14173315  |         1261857 | NBCNews         |
+| 2735591   |          960270 | FastCompany     |
+| 14055301  |          843075 | SavetheChildren |
+| 82629642  |          837694 | EslaCruelVerdad |
++-----------+-----------------+-----------------+
+
