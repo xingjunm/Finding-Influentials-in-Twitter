621e62df06651710ec5cbeae1936db1b529ec0af
Xingjun Ma <danxjma@gmail.com>
2022-03-23 15:16:05 +0800

    Delete statistical anlaysis.py

--- temporal analysis/temporal pattern mining/statistical anlaysis.py
+++
@@ -1,7 +0,0 @@
-1. hour-based analaysis
-mysql> select hour,sum(total_count) as total from tweet_count_hour_avg group by hour;
-24 rows in set (0.08 sec)
-
-2. day-based analysis
-mysql> select day,sum(total_count) as total from tweet_count_day group by day;
-7 rows in set (0.08 sec)
